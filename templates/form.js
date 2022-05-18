1ed6ee717a4192e259460bb926bad808630d3cc8
rafee ali <rafee.ali@mail.utoronto.ca>
2022-05-17 22:36:35 -0400

    CSS&JS&HTML5 files added

---
+++ templates/form.js
@@ -0,0 +1,15 @@
+document.querySelector('form').onsubmit = function() {
+            
+    if (!document.querySelector('input').value)
+    {
+        alert('You must provide your name')
+        return false;
+    }
+    else if (!document.querySelector('select').value) 
+    {
+        alert('You must provide your dorm!')
+        return false;
+    }
+    return true;
+};
+
